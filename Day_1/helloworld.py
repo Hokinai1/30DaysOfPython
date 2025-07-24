f22aaa7158582df851df6c5a0916e120c1f3518c
Hokinai1 <hokinaiesso@gmail.com>
2025-07-24 00:28:33 +0000

    Ajout du dossier d'exercice du jour 1

---
+++ Day_1/helloworld.py
@@ -0,0 +1,55 @@
+#Day 1 - 30 DaysOfPython Challenge
+
+print( 3 + 4)  #addition
+print(3 - 4)  #soustraction
+print(3 * 4) #Multiplication
+print(3 % 4) #Modulo
+print(3**4) #Exponentielle
+print(3//4) #Floor division
+
+print('Hokinai')
+print('Togo')
+print("I'm enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Hokinai'))
+print(type('Togo'))
+
+
+
+#Integer
+age = 10
+print(age)
+
+#Float
+distance = 5.75
+print(distance)
+
+#Complex
+complex_num = 2 + 5j
+print(complex_num)
+
+#String
+name = 'Hokinai'
+print(name)
+
+#Boolean
+is_valid = True
+print(is_valid)
+
+#List
+
+fruits = ['mangue', 'banane', 'orange', 'papaye']
+print(fruits)
+
+#Tuple
+
+
+#Dictionnaire
+moto = {"name": "Haoujue", "color": "red", "is_good": True, "number": 20}
+print(moto)
+
