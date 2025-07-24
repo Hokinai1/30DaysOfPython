c3b5b956dd521eee5ef87310b5395beff4f88c72
Hokinai1 <hokinaiesso@gmail.com>
2025-07-24 00:09:42 +0000

    premier cours: base sur python

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+#Day 1 - 30 DaysOfPython Challenge
+
+print( 3 + 4)  #addition
+print(3 - 4)  #soustraction
+print(3 * 4) #Multiplication
+print(3 % 4) #Modulo
+print(3**4) #Exponentielle
+print(3//4) #Floor division
+
+print('Hokinai')
+print('Togo')
+print("I'm enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Hokinai'))
+print(type('Togo'))
+
